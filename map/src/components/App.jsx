29e18e6624dc03d7bdd4f54c82ea94ebea9c344a
Anton Pashkouski <Wolendrang@gmail.com>
2018-09-24 16:50:51 +0300

    Multilayer map updated

--- map/src/components/App.jsx
+++ map/src/components/App.jsx
@@ -6,10 +6,24 @@ import MultilayerMap from "./MultilayerMap.jsx";
 import "./Reset.less";
 import "./App.less";
 
+const RegularNodePopup = ({ information }) => {
+    return <span>{information}</span>;
+};
+
+const LeafNodePopup = ({ information }) => {
+    return (
+        <div>
+            Longtitude: {information.longtitude},
+            Latitude: {information.latitude}
+        </div>
+    );
+};
+
 export default class App extends Component {
     render() {
         const config = {
-            nodeStyle: {
+            token: "pk.eyJ1IjoiYW50b24tcGFzaGtvdXNraSIsImEiOiJjam1hYjduZnUwOGphM3BvZ3Vwc3hsM3NrIn0.uP22N1el75_qWLJnCtAJXg",
+            regularNodeStyle: {
                 type: "circle",
                 properties: {
                     radius: 12,
@@ -39,49 +53,116 @@ export default class App extends Component {
                     {
                         entryPoint: [-94.565559, 39.015697],
                         coordinates: [
-                            [-94.565559, 39.015697],
-                            [-90.199402, 38.627003],
-                            [-93.258133, 44.986656]
+                            [-94.665559, 39.215697],
+                            [-94.365559, 39.315697],
+                            [-94.865559, 39.815697],
                         ],
                         subLayers: []
                     },
                     {
                         entryPoint: [-90.199402, 38.627003],
                         coordinates: [
-                            [-94.565559, 39.015697],
-                            [-90.199402, 38.627003],
-                            [-93.258133, 44.986656]
+                            [-90.899402, 38.927003],
+                            [-90.399402, 38.827003],
+                            [-90.199402, 38.227003]
                         ],
                         subLayers: []
                     },
                     {
                         entryPoint: [-93.258133, 44.986656],
                         coordinates: [
-                            [-94.565559, 39.015697],
-                            [-90.199402, 38.627003],
-                            [-93.258133, 44.986656]
+                            [-93.458133, 44.186656],
+                            [-93.258133, 44.586656],
+                            [-93.858133, 44.986656]
                         ],
                         subLayers: []
                     }
                 ]
             },
-            leafInfo: [
+            regularNodesInformation: [
                 {
                     coordinate: [-94.565559, 39.015697],
-                    info: {
-                        longtitude: 80,
-                        latitude: 90
+                    information: "Kansas City"
+                },
+                {
+                    coordinate: [-90.199402, 38.627003],
+                    information: "St. Louis"
+                },
+                {
+                    coordinate: [-93.258133, 44.986656],
+                    information: "Minneapolis"
+                },
+            ],
+            leafNodesInformation: [
+                {
+                    coordinate: [-94.665559, 39.215697],
+                    information: {
+                        longtitude: 1,
+                        latitude: 2
+                    }
+                },
+                {
+                    coordinate: [-94.365559, 39.315697],
+                    information: {
+                        longtitude: 3,
+                        latitude: 4
+                    }
+                },
+                {
+                    coordinate: [-94.865559, 39.815697],
+                    information: {
+                        longtitude: 5,
+                        latitude: 6
+                    }
+                },
+                {
+                    coordinate: [-90.899402, 38.927003],
+                    information: {
+                        longtitude: 7,
+                        latitude: 8
+                    }
+                },
+                {
+                    coordinate: [-90.399402, 38.827003],
+                    information: {
+                        longtitude: 9,
+                        latitude: 10
+                    }
+                },
+                {
+                    coordinate: [-90.199402, 38.227003],
+                    information: {
+                        longtitude: 11,
+                        latitude: 12
+                    }
+                },
+                {
+                    coordinate: [-93.458133, 44.186656],
+                    information: {
+                        longtitude: 13,
+                        latitude: 14
+                    }
+                },
+                {
+                    coordinate: [-93.258133, 44.586656],
+                    information: {
+                        longtitude: 15,
+                        latitude: 16
+                    }
+                },
+                {
+                    coordinate: [-93.858133, 44.986656],
+                    information: {
+                        longtitude: 17,
+                        latitude: 18
                     }
                 }
             ],
-            leafInfoRender: (listInfo) => {
-                return (
-                    <div>
-                        Longtitude: {listInfo.longtitude},
-                        Latitude: {listInfo.latitude}
-                    </div>
-                );
-            }
+            regularNodeInfoRender: regularNodeInfo => {
+                return null;
+            },
+            regularNodePopup: RegularNodePopup,
+            leafNodePopup: LeafNodePopup
         };
 
         return (
--- map/src/components/MultilayerMap.jsx
+++ map/src/components/MultilayerMap.jsx
@@ -1,20 +1,106 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import { ParentSize } from "@vx/responsive";
-import ReactMapboxGl, { Layer, Feature, Cluster, Marker } from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Feature, Popup, ZoomControl } from "react-mapbox-gl";
 
 import "./MultilayerMap.less";
 
 export default class MultilayerMap extends PureComponent {
 	constructor(props) {
 		super(props);
+
+		this.getInitialStateObject = this.getInitialStateObject.bind(this);
 		this.renderMap = this.renderMap.bind(this);
+		this.getSubLayer = this.getSubLayer.bind(this);
+		this.isLeafNode = this.isLeafNode.bind(this);
+		this.onRegularNodeClick = this.onRegularNodeClick.bind(this);
+		this.onZoomControlClick = this.onZoomControlClick.bind(this);
+		this.goToSublayer = this.goToSublayer.bind(this);
+		this.goToParentLayer = this.goToParentLayer.bind(this);
+		this.onMouseEnterOnNode = this.onMouseEnterOnNode.bind(this);
+		this.onMouseLeaveNode = this.onMouseLeaveNode.bind(this);
+		this.splitCoordinatesOnRegularAndLeaf = this.splitCoordinatesOnRegularAndLeaf.bind(this);
+
+		const { config } = this.props;
+		this.state = this.getInitialStateObject(this.props.config);
+
+		this.mapComponent = ReactMapboxGl({
+			accessToken: config.token,
+			scrollZoom: false,
+			doubleClickZoom: false
+		});
 	}
 
-	componentDidMount() {
+	componentWillReceiveProps(nextProps) {
+		this.setState(this.getInitialStateObject(nextProps.config));
 	}
 
-	componentDidUpdate() {
+	splitCoordinatesOnRegularAndLeaf(layer) {
+		return {
+			regularNodeCoordinates: _.filter(layer.coordinates, coordinate => !this.isLeafNode(layer, coordinate)),
+			leafNodeCoordinates: _.filter(layer.coordinates, coordinate => this.isLeafNode(layer, coordinate)),
+		};
+	}
+
+	getInitialStateObject(config) {
+		const rootLayer = config.rootLayer;
+		const { regularNodeCoordinates, leafNodeCoordinates } = this.splitCoordinatesOnRegularAndLeaf(rootLayer);
+
+		return {
+			currentLayer: rootLayer,
+			regularNodeStyle: this.transformStyleFormat(config.regularNodeStyle),
+			leafNodeStyle: this.transformStyleFormat(config.leafNodeStyle),
+			regularNodeCoordinates,
+			leafNodeCoordinates,
+			parentLayers: [],
+			popupInformation: null
+		};
+	}
+
+	goToSublayer(currentLayer, subLayer) {
+		let parentLayers = this.state.parentLayers || [];
+		const { regularNodeCoordinates, leafNodeCoordinates } = this.splitCoordinatesOnRegularAndLeaf(subLayer);
+
+		parentLayers.push(currentLayer);
+
+		this.setState({
+			parentLayers,
+			currentLayer: subLayer,
+			regularNodeCoordinates,
+			leafNodeCoordinates,
+			popupInformation: null
+		});
+	}
+
+	goToParentLayer() {
+		let parentLayers = this.state.parentLayers || [];
+		let parentLayer = parentLayers.pop();
+		const { regularNodeCoordinates, leafNodeCoordinates } = this.splitCoordinatesOnRegularAndLeaf(parentLayer);
+
+		if (parentLayer) {
+			this.setState({
+				parentLayers,
+				currentLayer: parentLayer,
+				regularNodeCoordinates,
+				leafNodeCoordinates,
+				popupInformation: null
+			});
+		}
+	}
+
+	transformStyleFormat(apiFormattedStyle) {
+		const properties = apiFormattedStyle.properties;
+
+		return {
+			type: apiFormattedStyle.type,
+			paint: {
+				"circle-radius": properties.radius,
+				"circle-color": properties.color,
+				"circle-stroke-width": properties.strokeWidth,
+				"circle-stroke-color": properties.strokeColor,
+				"circle-stroke-opacity": properties.strokeOpacity
+			}
+		};
 	}
 
 	getBoundsCoordinates(coordinates) {
@@ -34,42 +120,139 @@ export default class MultilayerMap extends PureComponent {
 		];
 	}
 
-	renderMap(width, height, config) {
-		const Map = ReactMapboxGl({
-			accessToken: "pk.eyJ1IjoiYW50b24tcGFzaGtvdXNraSIsImEiOiJjam1hYjduZnUwOGphM3BvZ3Vwc3hsM3NrIn0.uP22N1el75_qWLJnCtAJXg",
-			scrollZoom: false,
-			doubleClickZoom: false
-		});
+	isCoordinatesEqual(firstCoordinate, secondCoordinate) {
+		return firstCoordinate[0] === secondCoordinate[0] &&
+			firstCoordinate[1] === secondCoordinate[1];
+	}
+
+	getSubLayer(layer, coordinate) {
+		return _.find(layer.subLayers, subLayer => this.isCoordinatesEqual(subLayer.entryPoint, coordinate));
+	}
+
+	isLeafNode(layer, coordinate) {
+		return !this.getSubLayer(layer, coordinate);
+	}
+
+	onRegularNodeClick(coordinate) {
+		const { currentLayer } = this.state;
+		const subLayer = this.getSubLayer(currentLayer, coordinate);
+
+		if (subLayer) {
+			this.goToSublayer(currentLayer, subLayer);
+		}
+	}
+
+	onZoomControlClick(map, zoomDiff) {
+		if (zoomDiff < 0) {
+			this.goToParentLayer();
+		}
+	}
+
+	onMouseEnterOnNode(coordinate) {
+		const { currentLayer } = this.state;
+		const isLeafNode = this.isLeafNode(currentLayer, coordinate);
+		let nodesInformation = isLeafNode ?
+			this.props.config.leafNodesInformation :
+			this.props.config.regularNodesInformation;
+
+		if (nodesInformation && nodesInformation.length) {
+			const nodeInformation = _.find(nodesInformation, info => this.isCoordinatesEqual(info.coordinate, coordinate));
+
+			if (nodeInformation) {
+				this.setState({
+					popupInformation: {
+						coordinate: nodeInformation.coordinate,
+						information: nodeInformation.information,
+						isLeafNode: isLeafNode
+					}
+				});
+			}
+		}
+	}
+
+	onMouseLeaveNode() {
+		this.setState({ popupInformation: null });
+	}
+
+	onMapStyleLoad(currentBoundCoordinates, map) {
+		map.resize();
+		map.fitBounds(currentBoundCoordinates);
+	}
+
+	renderMap(width, height) {
+		let LeafNodePopup = this.props.config.leafNodePopup;
+		let RegularNodePopup = this.props.config.regularNodePopup;
+		const {
+			currentLayer,
+			regularNodeStyle,
+			regularNodeCoordinates,
+			leafNodeStyle,
+			leafNodeCoordinates,
+			popupInformation,
+			parentLayers
+		} = this.state;
+
+		const currentBoundCoordinates = this.getBoundsCoordinates(currentLayer.coordinates);
+		const Map = this.mapComponent;
+		const mapContainerStyle = {
+			width: `${width}px`,
+			height: `${height}px`
+		};
+
+		// Empty component if not specified
+		RegularNodePopup = RegularNodePopup || (() => null);
+		const PopupInformationControl = (popupInformation && popupInformation.isLeafNode) ? LeafNodePopup : RegularNodePopup;
 
 		return (
 			<Map
 				style="mapbox://styles/mapbox/streets-v9"
-				fitBounds={this.getBoundsCoordinates(config.rootLayer.coordinates)}
-				containerStyle={{
-					width: `${width}px`,
-					height: `${height}px`
-				}}>
-					<Layer type="circle" id="marker" paint={{
-						"circle-radius": 12,
-						"circle-color": "#D71920",
-						"circle-stroke-width": 4,
-						"circle-stroke-color": "#000000",
-						"circle-stroke-opacity": 0.5
-					}}>
-						{config.rootLayer.coordinates.map((coordinate, index) => (
-							<Feature key={index} coordinates={coordinate}/>
-						))}
-					</Layer>
+				fitBounds={currentBoundCoordinates}
+				onStyleLoad={this.onMapStyleLoad.bind(this, currentBoundCoordinates)}
+				containerStyle={mapContainerStyle}>
+
+					{regularNodeCoordinates.length &&
+						<Layer type={regularNodeStyle.type} id="regularNodesLayer" paint={regularNodeStyle.paint}>
+							{regularNodeCoordinates.map((coordinate, index) => (
+								<Feature
+									key={index}
+									coordinates={coordinate}
+									onClick={this.onRegularNodeClick.bind(this, coordinate)}
+									onMouseEnter={this.onMouseEnterOnNode.bind(this, coordinate)}
+									onMouseLeave={this.onMouseLeaveNode}/>
+							))}
+						</Layer>
+					}
+
+					{leafNodeCoordinates.length &&
+						<Layer type={leafNodeStyle.type} id="leafNodesLayer" paint={leafNodeStyle.paint}>
+							{leafNodeCoordinates.map((coordinate, index) => (
+								<Feature
+									key={index}
+									coordinates={coordinate}
+									onMouseEnter={this.onMouseEnterOnNode.bind(this, coordinate)}
+									onMouseLeave={this.onMouseLeaveNode}/>
+							))}
+						</Layer>
+					}
+
+					{parentLayers.length && <ZoomControl onControlClick={this.onZoomControlClick}/>}
+
+					{popupInformation &&
+						<Popup
+							coordinates={popupInformation.coordinate}
+							anchor="bottom"
+							offset={{"bottom": [0, -this.props.config.leafNodeStyle.properties.radius]}}>
+							<PopupInformationControl information={popupInformation.information}/>
+						</Popup>
+					}
 			</Map>
 		);
 	}
 
 	render() {
-		const { config } = this.props;
-
 		return (
 			<ParentSize>
-				{parent => this.renderMap(parent.width, parent.height, config)}
+				{parent => this.renderMap(parent.width, parent.height)}
 			</ParentSize>
 		);
 	}
